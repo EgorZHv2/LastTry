36eb73cb66b8d680d6fd028f43356421bd21b699
Егор <egor.zholobov@yandex.ru>
2023-06-19 19:31:24 +0300

    AddFiles

--- WpfApp/WpfApp/MainWindow.xaml.cs
+++ WpfApp/WpfApp/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp.Data;
 
 namespace WpfApp
 {
@@ -23,6 +26,7 @@ namespace WpfApp
         public MainWindow()
         {
             InitializeComponent();
+       
         }
     }
 }
